71f92e7aa58d86b87ac1e83e1b600ae2e8c1294a
unknown <adewunmiadesokan@gmail.com>
2014-10-31 08:49:33 +0100

    proj

--- QuizHub/app/controllers/first_page_controller.rb
+++
@@ -1,5 +0,0 @@
-class FirstPageController < ApplicationController
-	layout 'temp'
-  def first_page
-  end
-end
--- app/controllers/first_page_controller.rb
+++ app/controllers/first_page_controller.rb
@@ -1,5 +1,18 @@
 class FirstPageController < ApplicationController
 	layout 'temp'
+  
   def first_page
   end
+
+  # def show
+  #   @title = title.find(params[:id])
+  #   @category = category.find(params[:id])
+  # end
+  
+# def first_page_params
+
+#   	params.require(:quiz).permit(:title, :category)
+
+# end
+
 end
